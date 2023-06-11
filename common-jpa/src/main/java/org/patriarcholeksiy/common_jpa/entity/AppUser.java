bddd6a18be97fabf66c8ea25dda29772e9e7fef7
patriarcholeksiy <kataaas@yandex.ru>
2023-06-11 18:33:06 +0300

    Created class AppUser

---
+++ common-jpa/src/main/java/org/patriarcholeksiy/common_jpa/entity/AppUser.java
@@ -0,0 +1,25 @@
+package org.patriarcholeksiy.common_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+}
